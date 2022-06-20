085177d2f6bbfd19f67b6f9c827b48058cf336cd
Ferdianfy13 <ferdianfy13@gmail.com>
2022-06-20 09:05:09 +0700

    Training Inheritance

---
+++ Training5/main.py
@@ -0,0 +1,13 @@
+from Hero import HeroIntelligent, HeroStrength
+
+imil = HeroIntelligent("Imil")
+lactiva = HeroStrength("Lactiva")
+
+imil.show_info()
+lactiva.show_info()
+
+imil.gainExp = 200
+lactiva.gainExp = 350
+
+imil.show_info()
+lactiva.show_info()
